6cb914af43022fb97a4807478547d1a435146d18
MMuzammalAbbas <chemistrymath984@gmail.com>
2025-06-12 11:53:55 +0500

    :fire: Ai Guru started!

---
+++ src/types/three-examples.d.ts
@@ -0,0 +1,6 @@
+declare module 'three/examples/jsm/loaders/GLTFLoader' {
+  import { Loader } from 'three';
+  export class GLTFLoader extends Loader {
+    // You can add more specific types if needed, but this is enough for most use
+  }
+}
